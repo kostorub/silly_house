1a5e6c7122057d273182bbae97dfa6f3345c0543
Vasiliy Kostrubov <v.kostrubov@hotmail.com>
2021-04-10 22:10:17 +0100

    configure.sh & requirements.txt updated

--- configure.sh
+++ configure.sh
@@ -3,7 +3,8 @@ CUR_DIR=`pwd`
 
 sudo apt update && \ 
 sudo apt upgrade -y && \
-sudo apt install -y python3-venv python3-dev libatlas-base-dev libwebp-dev libtiff5 libopenjp2-7 libilmbase23 libopenexr-dev ffmpeg libgtk-3-0 libgpiod-dev && \
+sudo apt install -y python3-venv python3-dev libatlas-base-dev libwebp-dev libtiff5 libopenjp2-7 libilmbase23 libopenexr-dev \
+ffmpeg libgtk-3-0 libgpiod-dev portaudio19-dev && \
 python3 -m venv venv && \
 $CUR_DIR/venv/bin/python3 -m pip install -U pip setuptools wheel && \
 $CUR_DIR/venv/bin/python3 -m pip install -r requirements.txt
