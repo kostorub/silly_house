081b6225bd05d1aafa5ca1d0fddc9e0989fc8b9d
Vasiliy Kostrubov <v.kostrubov@hotamil.com>
2021-08-29 14:44:36 +0300

    educating script added

---
+++ educating_433relay.py
@@ -0,0 +1,13 @@
+from rpi_rf import RFDevice
+from time import sleep
+
+
+code_on = 16777200
+code_off = 16777201
+
+rf_device = RFDevice(17, tx_pulselength=433)
+rf_device.enable_tx()
+
+rf_device.tx_code(code_on)
+sleep(0.8)
+rf_device.tx_code(code_off)
