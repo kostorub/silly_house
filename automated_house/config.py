c22286f4e0e3a56d38319b85cb9f8a21e14f9714
kostorub <v.kostrubov@hotamil.com>
2021-08-29 13:56:22 +0300

    checking of the CONFIG_PATH added

--- automated_house/config.py
+++ automated_house/config.py
@@ -3,4 +3,7 @@ from models.configuration import Configuration
 
 
 config_path = os.environ.get("CONFIG_PATH")
-config = Configuration(config_path, "configuration.yaml")
+if config_path:
+    config = Configuration(config_path, "configuration.yaml")
+else:
+    raise Exception("Set CONFIG_PATH env variable which contains configuration.yaml on this path")
